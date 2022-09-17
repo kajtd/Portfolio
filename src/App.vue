3ee9e6d97b3d562d66019ddbd4709cb279956b09
Kajetan Domagala <kajtekd8@gmail.com>
2022-09-17 16:58:19 +0200

    feat: hide animations on prefers-reduced-motion

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
-<template>
-  <main class="w-full flex flex-col">
-    <AppOverlay />
-    <AppNavbar />
-    <HeroSection />
-  </main>
-</template>
-
 <script setup lang="ts">
 import AppNavbar from './components/AppNavbar.vue';
 import HeroSection from './components/HeroSection.vue';
 import AppOverlay from './components/AppOverlay.vue';
+import { useMedia } from './composables/useMedia';
+
+const isReducedMotion = useMedia('(prefers-reduced-motion)');
 </script>
+
+<template>
+  <main class="w-full">
+    <AppOverlay v-if="!isReducedMotion" />
+    <div class="min-h-screen h-full flex flex-col">
+      <AppNavbar />
+      <HeroSection />
+    </div>
+  </main>
+</template>
