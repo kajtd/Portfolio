539db59274c9bd7692e801b516c6997e46ea3a2c
Kajetan Domagala <kajtekd8@gmail.com>
2022-09-22 20:14:16 +0200

    feat: start creating section with sample projects

---
+++ src/components/SampleProjects/TheProject.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+interface Props {
+  imageName: string;
+  title: string;
+  description: string;
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+  <div>
+    <img
+      :src="`images/${imageName}.png`"
+      :alt="imageName"
+      class="max-w-2xl w-full"
+    />
+    <h3 class="text-2xl sm:text-[32px] font-medium mt-10 mb-4">
+      {{ title }}
+    </h3>
+    <p class="text-base sm:text-xl">
+      {{ description }}
+    </p>
+  </div>
+</template>
