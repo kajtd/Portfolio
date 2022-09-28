525f50b6a0d40088751ef4a08c0e02d52ead09b7
Kajetan Domagala <kajtekd8@gmail.com>
2022-09-28 21:35:27 +0200

    feat(herosection.vue): add animations for hero section and fix minor bugs with this section

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -5,6 +5,7 @@ import { useMedia } from '../composables/useMedia';
 
 const isReducedMotion = useMedia('(prefers-reduced-motion)');
 const title = ref<HTMLHeadingElement>();
+const heroText = ref<HTMLDivElement>();
 
 onMounted(() => {
   if (!isReducedMotion.value) {
@@ -16,7 +17,20 @@ onMounted(() => {
       },
       {
         duration: 1.5,
-        delay: 2.5,
+        delay: 3,
+        opacity: 1,
+        y: 0,
+      }
+    );
+    gsap.fromTo(
+      heroText.value as HTMLDivElement,
+      {
+        opacity: 0,
+        y: '100%',
+      },
+      {
+        duration: 1.5,
+        delay: 3,
         opacity: 1,
         y: 0,
       }
@@ -27,54 +41,34 @@ onMounted(() => {
 
 <template>
   <section
-    class="h-full flex-1 flex flex-col justify-center relative"
+    ref="heroSection"
+    class="h-full flex-1 flex flex-col justify-center relative px-6 md:px-8 xl:px-24"
   >
     <h1
       id="title"
       ref="title"
-      class="text-black text-[34px] md:text-[64px] leading-[60px] font-medium md:leading-[96px] max-w-5xl lg:ml-24 mb-24"
+      class="text-black text-[34px] md:text-[70px] leading-[60px] font-medium md:leading-[100px] xl:text-[90px] xl:leading-[120px] max-w-7xl lg:ml-24 mb-24"
     >
       <span
-        class="relative after:h-2 md:after:h-3 after:w-[98%] after:bg-primary after:absolute after:bottom-[-6px] md:after:-bottom-2 after:left-0"
+        class="inline-block relative after:h-1 md:after:h-2 after:w-[50%] after:bg-primary after:absolute after:-bottom-1 md:after:-bottom-2 after:left-0"
       >
         Frontend developer
       </span>
       passionate about creating web applications
     </h1>
     <div
-      class="hidden xs:flex bottom-12 right-0 absolute sm:bottom-24 sm:right-16 xl:!bottom-20 xl:!right-20"
+      ref="heroText"
+      class="hidden xs:flex bottom-20 right-0 absolute sm:bottom-24 sm:right-16 xl:!bottom-20 xl:!right-20"
     >
       <img
         src="./../assets/icons/arrow.svg"
         alt=""
         class="w-24 relative top-14 left-2"
       />
-      <p class="text-black text-base max-w-[150px]">
+      <p class="text-black text-base md:text-xl max-w-[250px]">
         View the projects in my portfolio to learn more about who I
         am.
       </p>
-    </div>
-    <div
-      class="absolute bottom-0 left-4 md:left-8 xl:left-0 flex flex-col items-center gap-2"
-    >
-      <a href="https://github.com/kajtd" target="_blank">
-        <img
-          src="./../assets/icons/github.svg"
-          alt=""
-          class="w-8 md:w-12 custom-icon"
-        />
-      </a>
-      <a
-        href="https://www.linkedin.com/in/kajetan-domagala/"
-        target="_blank"
-      >
-        <img
-          src="./../assets/icons/linkedin.svg"
-          alt=""
-          class="w-8 md:w-12 custom-icon"
-        />
-      </a>
-      <div class="h-16 w-2 bg-black"></div>
     </div>
   </section>
 </template>
