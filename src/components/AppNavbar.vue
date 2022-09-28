108b3b077315413221db0bd5b3435f270da1b1bc
Kajetan Domagala <kajtekd8@gmail.com>
2022-09-28 21:29:28 +0200

    feat(appnavbar.vue): add animations for AppNavbar file

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
+import gsap from 'gsap';
 import HamburgerMenuButton from './HamburgerMenuButton.vue';
 import AppButton from './AppButton.vue';
+import { useMedia } from './../composables/useMedia';
 
 interface Link {
   name: string;
@@ -24,6 +26,42 @@ const links = ref<Link[]>([
 ]);
 
 const mobileNav = ref(false);
+const nav = ref<HTMLDivElement>();
+const logoWrapper = ref<HTMLDivElement>();
+const isReducedMotion = useMedia('(prefers-reduced-motion)');
+
+onMounted(() => {
+  if (!isReducedMotion.value) {
+    gsap.fromTo(
+      logoWrapper.value as HTMLDivElement,
+      {
+        opacity: 0,
+        delay: 2.5,
+        y: '100%',
+      },
+      {
+        duration: 1,
+        opacity: 1,
+        delay: 2.5,
+        y: 0,
+      }
+    );
+    gsap.fromTo(
+      nav.value as HTMLDivElement,
+      {
+        opacity: 0,
+        delay: 2.5,
+        y: '100%',
+      },
+      {
+        duration: 1,
+        opacity: 1,
+        delay: 2.5,
+        y: 0,
+      }
+    );
+  }
+});
 
 const toggleMobileNav = (): void => {
   mobileNav.value = !mobileNav.value;
@@ -33,10 +71,12 @@ const toggleMobileNav = (): void => {
 
 <template>
   <header
-    class="flex items-center justify-between w-full mx-auto mt-6"
+    id="home"
+    class="flex items-center justify-between w-full mx-auto pt-6 md:pt-12 px-4 md:px-8 xl:px-24"
   >
     <div class="w-full mx-auto flex items-center justify-between">
       <div
+        ref="logoWrapper"
         class="font-medium flex items-center leading-[78px] text-lg md:text-2xl"
       >
         <img
@@ -44,14 +84,17 @@ const toggleMobileNav = (): void => {
           alt=""
           class="mr-1 w-8 md:w-10"
         />
-        <span>Kajetan Domagała</span>
+        <span class="name">Kajetan Domagała</span>
       </div>
       <HamburgerMenuButton
         :checked="mobileNav"
         @toggle-mobile-nav="toggleMobileNav"
       />
-      <nav class="hidden xl:flex items-center justify-start">
-        <ul class="flex items-center gap-8">
+      <nav
+        ref="nav"
+        class="hidden xl:flex items-center justify-start"
+      >
+        <ul class="flex items-center gap-10">
           <li v-for="link in links" :key="link.name">
             <a :href="link.url" class="inline-block link">
               <span
@@ -69,7 +112,7 @@ const toggleMobileNav = (): void => {
       <transition name="mobile-nav">
         <nav
           v-show="mobileNav"
-          class="xl:hidden flex flex-col items-center justify-center h-full fixed bottom-0 left-0 p-8 w-full bg-white z-30"
+          class="xl:hidden text-center flex flex-col items-center justify-center h-full fixed bottom-0 left-0 p-8 w-full bg-white z-30"
         >
           <ul>
             <li
@@ -79,17 +122,14 @@ const toggleMobileNav = (): void => {
             >
               <button @click="toggleMobileNav">
                 <a :href="link.url" class="inline-block">
-                  <span class="font-semibold text-xl">
+                  <span class="font-medium text-2xl">
                     {{ link.name }}
                   </span>
                 </a>
               </button>
             </li>
             <li>
-              <AppButton
-                href="#contact"
-                class="text-xl h-14 w-44 mt-40"
-              >
+              <AppButton href="#contact" class="mt-12">
                 contact
               </AppButton>
             </li>
