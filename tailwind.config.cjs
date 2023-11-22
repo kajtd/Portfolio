410df383c93d5eba29bd280422451442002721f8
Kajetan Domagala <kajtekd8@gmail.com>
2023-11-22 12:32:11 +0100

    Change the primary color to purple

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,22 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       screens: {
-        "xs": "480px"
+        xs: "480px",
       },
       boxShadow: {
-        'outlined-primary-shadow': '0 0 0 12px #fcdda3'
+        "outlined-primary-shadow": "0 0 0 12px #9966CC",
+        "outlined-primary-shadow-small": "0 0 0 4px #9966CC",
       },
       colors: {
-        primary: '#FCDDA3',
-        background: '#FEFDF1'
-      }
+        primary: "#9966CC",
+        background: "#FEFDF1",
+      },
     },
   },
   plugins: [],
-}
+};
