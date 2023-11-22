a0cf26220cc5e339d4699ffb9db5e05b484de40a
Kajetan Domagala <kajtekd8@gmail.com>
2023-11-22 12:32:31 +0100

    Create a new contact section

--- src/components/ContactMe.vue
+++ src/components/ContactMe.vue
@@ -1,18 +1,28 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
-import AppButton from './AppButton.vue';
-import { useIntersectionObserver } from './../composables/useIntersectionObserver';
+import { ref, onMounted } from "vue";
+import AppButton from "./AppButton.vue";
+import { useIntersectionObserver } from "./../composables/useIntersectionObserver";
+import gsap from "gsap";
 
-const { observedElement, elementClass, observer } =
-  useIntersectionObserver();
-
-const contactText = ref<Element>();
+const contactHeading = ref<Element>();
 
 const currentYear = ref(new Date().getFullYear());
 
+const animateContactHeading = () => {
+  gsap.to(".contact-char", {
+    y: 0,
+    stagger: 0.05,
+    duration: 0.1,
+  });
+};
+
+const { observedElement, elementClass, observer } = useIntersectionObserver(
+  animateContactHeading
+);
+
 onMounted(() => {
-  observedElement.value = contactText.value;
-  elementClass.value = 'fadein--animation';
+  observedElement.value = contactHeading.value;
+  elementClass.value = "stagger--animation";
   observer.observe(observedElement.value as Element);
 });
 </script>
@@ -23,31 +33,27 @@ onMounted(() => {
     class="relative bg-black text-white min-h-screen h-full flex flex-col pt-24 xl:pt-48 px-6 pb-1 md:px-8 xl:px-24"
   >
     <div
-      class="flex flex-col xl:flex-row items-center justify-around w-full"
+      class="flex flex-col lg:flex-row items-center justify-around w-full mb-36 gap-24 lg:gap-0"
     >
-      <div ref="contactText" class="fadein">
-        <h2 class="text-base xl:text-2xl font-semibold">
-          contact me
-        </h2>
-        <div class="flex mb-24 xl:mb-0">
-          <h3
-            class="text-4xl leading-[48px] xl:text-[62px] xl:leading-[92px] max-w-6xl"
+      <h2
+        class="contactHeading uppercase text-[90px] leading-[90px] md:text-[150px] md:leading-[120px] 2xl:!text-[240px] 2xl:leading-[196px] text-white lg:self-end"
+        ref="contactHeading"
+      >
+        <div
+          class="word inline-block mr-2"
+          v-for="(word, wordIndex) in 'contact'.split(' ')"
+          :key="`word-${wordIndex}`"
+        >
+          <div
+            class="contact-char inline-block transition-transform duration-500 translate-y-64"
+            v-for="(char, charIndex) in word.split('')"
+            :key="`contact-char-${charIndex}`"
           >
-            I would love to
-            <span
-              class="inline-block relative after:h-1 after:xl:h-3 after:w-[98%] after:bg-primary after:absolute after:bottom-[-2px] after:xl:-bottom-1 after:left-0"
-            >
-              hear from you
-            </span>
-            if you have any comments or questions.
-          </h3>
-          <img
-            src="./../assets/icons/arrow-white.svg"
-            alt=""
-            class="relative right-10 hidden xl:block"
-          />
+            {{ char }}
+          </div>
         </div>
-      </div>
+      </h2>
+
       <form
         class="flex flex-col w-full max-w-[500px]"
         action="https://formsubmit.co/kajetan.domagala00@gmail.com"
@@ -84,40 +90,15 @@ onMounted(() => {
         </div>
         <input type="hidden" name="_template" value="table" />
         <input type="text" name="_honey" class="hidden" />
-        <AppButton type="submit">Contact</AppButton>
+        <AppButton type="submit">Submit</AppButton>
       </form>
     </div>
 
     <div class="w-full mt-auto">
-      <div class="flex items-center gap-3 md:gap-6 mb-4 mt-36">
-        <a
-          href="https://github.com/kajtd"
-          target="_blank"
-          aria-label="Github"
-        >
-          <img
-            src="./../assets/icons/github-white.svg"
-            alt=""
-            class="custom-icon"
-          />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/kajetan-domagala/"
-          target="_blank"
-          aria-label="Linkedin"
-        >
-          <img
-            src="./../assets/icons/linkedin-white.svg"
-            alt=""
-            class="custom-icon"
-          />
-        </a>
-      </div>
       <p
         class="border-t border-solid border-gray-300 w-full grid place-items-center py-4 text-gray-300 text-sm text-center"
       >
-        Copyright {{ currentYear }} Kajetan Domagała. All rights
-        reserved.
+        Copyright {{ currentYear }} Kajetan Domagała. All rights reserved.
       </p>
     </div>
   </footer>
@@ -126,5 +107,8 @@ onMounted(() => {
 <style scoped>
 .form-box:focus ~ .line {
   width: 100%;
+}
+.contactHeading {
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 0% 100%);
 }
 </style>
