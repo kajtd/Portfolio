6937f064c79d70eb21b9cffbb4e50fb2cb592b85
Kajetan Domagala <kajtekd8@gmail.com>
2023-11-27 15:39:24 +0100

    Update AppNavbar

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
-import gsap from 'gsap';
-import HamburgerMenuButton from './HamburgerMenuButton.vue';
-import AppButton from './AppButton.vue';
-import { useMedia } from './../composables/useMedia';
+import { ref, onMounted } from "vue";
+import gsap from "gsap";
+import HamburgerMenuButton from "./HamburgerMenuButton.vue";
+import AppButton from "./AppButton.vue";
+import { useMedia } from "./../composables/useMedia";
 
 interface Link {
   name: string;
@@ -12,56 +12,45 @@ interface Link {
 
 const links = ref<Link[]>([
   {
-    name: 'home',
-    url: '#home',
+    name: "home",
+    url: "#home",
   },
   {
-    name: 'about',
-    url: '#about',
+    name: "about",
+    url: "#about",
   },
   {
-    name: 'projects',
-    url: '#projects',
+    name: "projects",
+    url: "#projects",
+  },
+  {
+    name: "contact",
+    url: "#contact",
   },
 ]);
 
 const mobileNav = ref(false);
 const nav = ref<HTMLDivElement>();
-const logoWrapper = ref<HTMLDivElement>();
-const isReducedMotion = useMedia('(prefers-reduced-motion)');
+const isReducedMotion = useMedia("(prefers-reduced-motion)");
 
 const handleRedirect = (url: string) => {
   window.location.href = url;
-  mobileNav.value = false;
+  toggleMobileNav();
 };
 
 onMounted(() => {
   if (!isReducedMotion.value) {
     gsap.fromTo(
-      logoWrapper.value as HTMLDivElement,
-      {
-        opacity: 0,
-        delay: 2.5,
-        y: '100%',
-      },
-      {
-        duration: 1,
-        opacity: 1,
-        delay: 2.5,
-        y: 0,
-      }
-    );
-    gsap.fromTo(
       nav.value as HTMLDivElement,
       {
         opacity: 0,
-        delay: 2.5,
-        y: '100%',
+        delay: 0.5,
+        y: "20px",
       },
       {
         duration: 1,
         opacity: 1,
-        delay: 2.5,
+        delay: 0.5,
         y: 0,
       }
     );
@@ -70,89 +59,71 @@ onMounted(() => {
 
 const toggleMobileNav = (): void => {
   mobileNav.value = !mobileNav.value;
-  document.body.classList.toggle('fixed-bg');
+  document.body.classList.toggle("fixed-bg");
 };
 </script>
 
 <template>
-  <header
-    id="home"
-    class="flex items-center justify-between w-full mx-auto pt-6 md:pt-16 px-4 md:px-8 xl:px-24"
-  >
-    <div class="w-full mx-auto flex items-center justify-between">
-      <div
-        ref="logoWrapper"
-        class="font-medium flex items-center leading-[78px] text-lg md:text-2xl"
-      >
-        <img
-          src="./../assets/icons/spiral.svg"
-          alt=""
-          class="mr-2 w-8 md:w-10"
-        />
-        <span class="name">Kajetan Domagała</span>
+  <div>
+    <header
+      ref="nav"
+      id="home"
+      class="flex items-center justify-between w-full mx-auto pt-12 px-4 md:px-8 xl:px-24 relative"
+    >
+      <div class="w-full mx-auto flex items-center justify-between">
+        <a href="/" class="text-xl xs:text-2xl md:text-4xl">Kajetan Domagała</a>
+        <nav class="flex items-center justify-start">
+          <ul class="flex items-center md:gap-10">
+            <li>
+              <a
+                href="#contact"
+                class="hidden md:inline-block link text-2xl leading-[78px] text-white transition-colors duration-250 hover:text-primary"
+              >
+                contact
+              </a>
+            </li>
+            <li>
+              <AppButton @click="toggleMobileNav"> menu </AppButton>
+            </li>
+          </ul>
+        </nav>
       </div>
-      <HamburgerMenuButton
-        :checked="mobileNav"
-        @toggle-mobile-nav="toggleMobileNav"
-      />
+    </header>
+    <transition name="mobile-nav">
       <nav
-        ref="nav"
-        class="hidden xl:flex items-center justify-start"
+        v-show="mobileNav"
+        class="flex flex-col justify-center h-full fixed inset-0 py-12 px-4 md:px-8 xl:px-24 w-full bg-black text-white z-30"
       >
-        <ul class="flex items-center gap-10">
-          <li v-for="link in links" :key="link.name">
-            <a :href="link.url" class="inline-block link">
+        <AppButton
+          class="z-50 !absolute top-12 right-4 md:right-0 xl:right-24"
+          @click="toggleMobileNav"
+        >
+          close
+        </AppButton>
+        <ul class="self-center md:self-start">
+          <li
+            v-for="link in links"
+            :key="link.name"
+            class="py-8 font-bold transition duration-300 cursor-pointer text-white"
+          >
+            <a class="inline-block" @click="handleRedirect(link.url)">
               <span
-                class="font-medium text-2xl leading-[78px] text-black"
+                class="font-medium uppercase text-5xl sm:text-7xl lg:text-[120px] lg:leading-[100px] hover:text-primary transition-colors"
               >
                 {{ link.name }}
               </span>
             </a>
           </li>
-          <li>
-            <AppButton href="#contact">contact</AppButton>
-          </li>
         </ul>
       </nav>
-      <transition name="mobile-nav">
-        <nav
-          v-show="mobileNav"
-          class="xl:hidden text-center flex flex-col items-center justify-center h-full fixed bottom-0 left-0 p-8 w-full bg-white z-30"
-        >
-          <ul>
-            <li
-              v-for="link in links"
-              :key="link.name"
-              class="text-3xl py-6 font-bold transition duration-300 cursor-pointer text-black"
-            >
-              <a
-                class="inline-block"
-                @click="handleRedirect(link.url)"
-              >
-                <span class="font-medium text-2xl">
-                  {{ link.name }}
-                </span>
-              </a>
-            </li>
-            <li>
-              <AppButton
-                class="mt-12"
-                @click="handleRedirect('#contact')"
-              >
-                contact
-              </AppButton>
-            </li>
-          </ul>
-        </nav>
-      </transition>
-    </div>
-  </header>
+    </transition>
+  </div>
 </template>
 
 <style scoped>
 .mobile-nav-enter-active,
 .mobile-nav-leave-active {
-  transition: all 0.4s ease-in-out;
+  transition: all 0.5s ease-in-out;
 }
 .mobile-nav-enter-from {
   transform: translateX(100%);
