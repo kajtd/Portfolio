9a0e8617dae978dd5c6270bce765112db6b3e308
Kajetan Domagala <kajtekd8@gmail.com>
2023-11-27 15:41:42 +0100

    Create the MVP of the Hero Section

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,75 +1,138 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
-import gsap from 'gsap';
-import { useMedia } from '../composables/useMedia';
+import { ref, onMounted, onUnmounted, computed } from "vue";
+import gsap from "gsap";
+import { useMedia } from "../composables/useMedia";
+import AppButton from "./AppButton.vue";
 
-const isReducedMotion = useMedia('(prefers-reduced-motion)');
-const title = ref<HTMLHeadingElement>();
-const heroText = ref<HTMLDivElement>();
+const isReducedMotion = useMedia("(prefers-reduced-motion)");
+const heroSection = ref<HTMLDivElement>();
+
+const mainHeaderSplit = computed(() => "Hi, I'm Kajetan".split(""));
+const scrollDown = ref<HTMLElement>();
+const hireMeButton = ref<HTMLElement>();
+
+const windowWidth = ref(window.innerWidth);
+
+const onResize = () => {
+  windowWidth.value = window.innerWidth;
+};
+const svgDimensions = computed(() => {
+  if (windowWidth.value < 768) {
+    return { width: 100, height: 100 };
+  }
+  return { width: 200, height: 200 };
+});
 
 onMounted(() => {
+  window.addEventListener("resize", onResize);
   if (!isReducedMotion.value) {
+    gsap.to(".main-header-char", {
+      y: 0,
+      stagger: 0.05,
+      delay: 1,
+      duration: 0.1,
+    });
     gsap.fromTo(
-      title.value as HTMLHeadingElement,
+      scrollDown.value as HTMLElement,
       {
         opacity: 0,
-        y: '100%',
       },
       {
         duration: 1.5,
-        delay: 3,
+        delay: 2,
         opacity: 1,
-        y: 0,
       }
     );
     gsap.fromTo(
-      heroText.value as HTMLDivElement,
+      hireMeButton.value as HTMLElement,
       {
         opacity: 0,
-        y: '100%',
       },
       {
         duration: 1.5,
-        delay: 3,
+        delay: 2,
         opacity: 1,
-        y: 0,
       }
     );
   }
+});
+
+onUnmounted(() => {
+  window.removeEventListener("resize", onResize);
 });
 </script>
 
 <template>
   <section
     ref="heroSection"
-    class="h-full flex-1 flex flex-col justify-center relative px-6 mt-12 md:px-8 xl:px-24"
+    class="h-full flex-1 flex items-center justify-start px-2 xs:px-6 md:px-8 xl:px-24"
   >
-    <h1
-      id="title"
-      ref="title"
-      class="text-black text-[34px] md:text-[70px] leading-[60px] font-medium md:leading-[100px] 2xl:text-[90px] 2xl:leading-[120px] max-w-7xl lg:ml-24 mb-24"
-    >
-      <span
-        class="inline-block relative z-[1] after:z-[-1] after:h-1 md:after:h-4 after:w-full after:bg-primary after:absolute after:bottom-2 md:after:bottom-0 after:left-0"
+    <div class="flex flex-col gap-12 text-left items-start ml-4 md:ml-24">
+      <h1
+        ref="mainHeader"
+        class="mainHeader font-medium text-white text-[34px] md:text-[70px] leading-[60px] md:leading-[100px] 2xl:text-[120px] 2xl:leading-[160px] max-w-7xl"
+        id="logo"
       >
-        Frontend
-      </span>
-      developer passionate about creating web applications.
-    </h1>
+        <div
+          class="word inline-block mr-1 md:mr-2"
+          v-for="(word, wordIndex) in mainHeaderSplit"
+          :key="`word-${wordIndex}`"
+        >
+          <div
+            class="main-header-char inline-block transition-transform duration-500 translate-y-48"
+            v-for="(char, charIndex) in word.split('')"
+            :key="`main-header-char-${charIndex}`"
+          >
+            {{ char }}
+          </div>
+        </div>
+      </h1>
+      <div ref="hireMeButton">
+        <AppButton>Hire Me</AppButton>
+      </div>
+    </div>
     <div
-      ref="heroText"
-      class="flex bottom-20 right-0 absolute sm:bottom-24 sm:right-16 xl:!bottom-20 xl:!right-20"
+      ref="scrollDown"
+      class="absolute bottom-4 right-6 md:right-20 grid place-items-center"
     >
       <img
-        src="./../assets/icons/arrow.svg"
+        class="absolute animate-[spin_3s_linear_infinite] duration-[5s] w-4 h-4 md:w-9 md:h-9"
+        src="./../assets/star.svg"
         alt=""
-        class="w-12 xs:w-24 relative top-10 xs:top-14 left-2"
       />
-      <p
-        class="text-black text-sm xs:text-base md:!text-xl max-w-[250px]"
+      <svg
+        id="rotatingText"
+        class="animate-[spin_10s_linear_infinite]"
+        viewBox="0 0 200 200"
+        :width="svgDimensions.width"
+        :height="svgDimensions.height"
       >
-        View the projects in my portfolio and learn more.
-      </p>
+        <defs>
+          <path
+            id="circle"
+            d="M 100, 100
+          m -75, 0
+          a 75, 75 0 1, 0 150, 0
+          a 75, 75 0 1, 0 -150, 0
+          "
+          ></path>
+        </defs>
+        <text width="400">
+          <textPath
+            alignment-baseline="top"
+            xlink:href="#circle"
+            class="font-light tracking-wider uppercase text-[23.9px] fill-current"
+          >
+            Learn more as you scroll down.
+          </textPath>
+        </text>
+      </svg>
     </div>
   </section>
 </template>
+
+<style scoped>
+.mainHeader {
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 0% 100%);
+}
+</style>
