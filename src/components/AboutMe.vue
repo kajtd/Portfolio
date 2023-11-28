ab462afe99dd2d6825e483ce76c02fb00da10ddb
Kajetan Domagala <kajtekd8@gmail.com>
2023-11-28 17:43:43 +0100

    Add HorizontalSlider and remove unnecessary about components

---
+++ src/components/AboutMe.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import HorizontalSlider from "./HorizontalSlider.vue";
+</script>
+<template>
+  <section
+    id="about"
+    class="min-h-screen flex flex-col relative overflow-hidden"
+  >
+    <HorizontalSlider>About</HorizontalSlider>
+  </section>
+</template>
