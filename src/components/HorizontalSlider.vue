e598e90ca4d6856abd1853715573a6c9985f6d8a
Kajetan Domagala <kajtekd8@gmail.com>
2024-01-05 17:33:23 +0100

    Fix issues with glitched horizontal slider

--- src/components/HorizontalSlider.vue
+++ src/components/HorizontalSlider.vue
@@ -7,10 +7,10 @@
         class="slider absolute top-0 left-0 overflow-hidden pointer-events-none"
       >
         <div
-          class="slider__content flex items-center relative w-fit h-32 md:h-48 opacity-0 transition-opacity"
+          class="slider__content flex items-center relative w-fit h-32 md:h-48"
         >
           <span
-            v-for="i in 8"
+            v-for="i in 16"
             :key="i"
             class="text-center whitespace-nowrap p-4 font-black leading-[100%]"
           >
@@ -24,17 +24,13 @@
 
 <style scoped>
 .slider__content {
-  transform: translate3d(-25%, 0, 0);
-  animation: sliding 5s linear infinite;
-}
-
-.slider .slider__content {
-  @apply opacity-100 duration-[400ms];
+  transform: translate3d(0, 0, 0);
+  animation: sliding 10s linear infinite;
 }
 
 @keyframes sliding {
   0% {
-    transform: translate3d(-25%, 0, 0);
+    transform: translate3d(0, 0, 0);
   }
 
   100% {
